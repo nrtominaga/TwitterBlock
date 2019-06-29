55c62eef37966f475e60c5600b49117d815d54be
Naoki Tominaga <u0876779@utah.edu>
2019-06-29 16:23:26 -0600

    loading keys

--- block.py
+++ block.py
@@ -1,2 +1,19 @@
 import requests
+import json
 
+KEYS_FILE = 'KEYS.json'
+
+
+def load_api_key(key_name, file_name=KEYS_FILE):
+    with open(file_name) as json_file:
+        return json.load(json_file)[key_name]
+
+
+API_KEY = load_api_key('API_KEY')
+API_SECRET_KEY = load_api_key('API_SECRET_KEY')
+ACCESS_TOKEN = load_api_key('ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = load_api_key('ACCESS_TOKEN_SECRET')
+
+
+if __name__ == "__main__":
+    print("Hello World")
