bc21a800a00967adb3217498a15721b3c8e7f370
Naoki Tominaga <u0876779@utah.edu>
2019-06-30 02:12:48 -0600

    done for the night

--- block.py
+++ block.py
@@ -6,6 +6,7 @@ KEYS_FILE = 'KEYS.json'
 BASE_URL = 'https://api.twitter.com/1.1'
 FOLLOWERS_ID_PATH = '/followers/ids.json'
 BLOCK_CREATE_PATH = '/blocks/create.json'
+BLOCK_DESTROY_PATH = '/blocks/destroy.json'
 
 
 def load_keys(key_name, file_name=KEYS_FILE):
@@ -38,11 +39,22 @@ def create_blocks(ids):
         create_block(id)
 
 
+def destory_block(id):
+    user_id_param = 'user_id='
+    url = BASE_URL + BLOCK_DESTROY_PATH + '?' + user_id_param + str(id)
+    requests.post(url, auth=AUTH)
+
+
 def destroy_blocks(ids):
     for id in ids:
+        destory_block(id)
 
+# TODO: get the blocks after blocking
+# TODO: cursoring
+# TODO: check for privacy settings
 
 
 if __name__ == "__main__":
     follower_ids = get_followers()['ids']
     create_blocks(follower_ids)
+    destroy_blocks(follower_ids)
